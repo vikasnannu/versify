81932ea04c5e9ee749131b779e4882186deeddeb
Vikas Kumar <vikasnannu@gmail.com>
2024-03-18 20:51:22 +0530

    cors config

--- server/configurations/socket.js
+++ server/configurations/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://versify-seven.vercel.app/",
+    origin: "https://versify-seven.vercel.app",
     methods: ["GET", "POST"],
   },
 });
