88d2eff35ec3af1dc8655be5e363ed9b09c0500d
Vikas Kumar <vikasnannu@gmail.com>
2024-03-20 21:00:47 +0530

    footer set

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -43,9 +43,6 @@ const LoginPage = () => {
           fontWeight="bold"
           color="gray.600"
           textAlign="center"
-          position="fixed"
-          bottom="0"
-          width="100%"
         >
           © 2024 Versify. All rights reserved.
         </Box>
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -41,9 +41,6 @@ const SignupPage = () => {
           fontWeight="bold"
           color="gray.600"
           textAlign="center"
-          position="fixed"
-          bottom="0"
-          width="100%"
         >
           © 2024 Versify. All rights reserved.
         </Box>
